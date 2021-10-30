17e646afda27bdb08e0ae4f968abe0eb921882a5
OGMorales17 <chorly786@gmail.com>
2021-10-30 16:11:15 -0400

    .

---
+++ eight-ball/src/App.js
@@ -0,0 +1,16 @@
+import EightBall from './EightBall';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Eight Ball</h1>
+        <EightBall />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
